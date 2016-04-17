b9fcad23c9fb96982f11d9f2413ffeefd1d64b14
TellH <570495627@qq.com>
2016-04-17 22:07:24 +0800

    增加CardView作为HeaderView的布局样式

---
+++ app/src/main/java/com/tellh/materialdesignlibrarydemo/coordinatorLayout/MaterialDesignScrollActivity.java
@@ -0,0 +1,18 @@
+package com.tellh.materialdesignlibrarydemo.coordinatorLayout;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tellh.materialdesignlibrarydemo.R;
+
+/**
+ * Created by tlh on 2016/4/17.
+ */
+public class MaterialDesignScrollActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coordinator_material_design);
+    }
+}
