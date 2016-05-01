137aa4de28fbf72863c59b99113755b59558133d
TellH <570495627@qq.com>
2016-05-01 19:48:40 +0800

    有一种实现底部Popupwindow的方式

--- app/src/main/java/com/tellh/materialdesignlibrarydemo/BottomSheet/BottomDialogActivity.java
+++ app/src/main/java/com/tellh/materialdesignlibrarydemo/BottomSheet/BottomDialogActivity.java
@@ -24,14 +24,17 @@ import net.soulwolf.widget.dialogbuilder.dialog.GridMasterDialog;
 import net.soulwolf.widget.dialogbuilder.dialog.ListMasterDialog;
 import net.soulwolf.widget.dialogbuilder.model.GridModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by tlh on 2016/4/12.
  */
-public class BottomDialogActivity extends AppCompatActivity implements View.OnClickListener, OnItemClickListener {
+public class BottomDialogActivity extends AppCompatActivity implements View.OnClickListener, OnItemClickListener, ListPopWindow.OnBottomTextviewClickListener, ListPopWindow.OnPopItemClickListener {
     private Button button;
     private Dialog mBottomSheetDialog;
     private Button anotherBotton;
-    private  ListMasterDialog dialog;
+    private ListMasterDialog dialog;
     private Button gridDialogButton;
     private GridMasterDialog gridDialog;
     private Button btnBottomSheetDlg;
@@ -47,10 +50,10 @@ public class BottomDialogActivity extends AppCompatActivity implements View.OnCl
 
     private void initView() {
         button = (Button) findViewById(R.id.button);
-        gridDialogButton=(Button)findViewById(R.id.button3);
-        anotherBotton=(Button)findViewById(R.id.button2);
-        btnBottomSheetDlg=(Button)findViewById(R.id.btn_bottom_sheet_dlg);
-        btnBottomSheetActivity=(Button)findViewById(R.id.button4);
+        gridDialogButton = (Button) findViewById(R.id.button3);
+        anotherBotton = (Button) findViewById(R.id.button2);
+        btnBottomSheetDlg = (Button) findViewById(R.id.btn_bottom_sheet_dlg);
+        btnBottomSheetActivity = (Button) findViewById(R.id.button4);
         btnBottomSheetActivity.setOnClickListener(this);
         button.setOnClickListener(this);
         anotherBotton.setOnClickListener(this);
@@ -108,7 +111,7 @@ public class BottomDialogActivity extends AppCompatActivity implements View.OnCl
                 .setOnItemClickListener(this)
                 .setGravity(net.soulwolf.widget.dialogbuilder.Gravity.BOTTOM);
         dialog = new ListMasterDialog(builder);
-        dialog.setAdapter(new TextDialogAdapter(this,getSimpleStringData(4)));
+        dialog.setAdapter(new TextDialogAdapter(this, getSimpleStringData(4)));
 
 
         gridDialog = new GridMasterDialog(builder);
@@ -116,28 +119,30 @@ public class BottomDialogActivity extends AppCompatActivity implements View.OnCl
         gridDialog.setNumColumns(4);
 
 
-        bottomSheetDlg=new BottomSheetDialog(this);
+        bottomSheetDlg = new BottomSheetDialog(this);
         View contentView = getLayoutInflater().inflate(R.layout.bottom_dialog, null);
         bottomSheetDlg.setContentView(contentView);
     }
 
-    private GridModel[] getSimpleGridModelData(){
+    private GridModel[] getSimpleGridModelData() {
         return new GridModel[]{
-                new GridModel(R.drawable.share_layout_qq_icon,"QQ"),
-                new GridModel(R.drawable.share_layout_wx_icon,"微信"),
-                new GridModel(R.drawable.share_layout_friends_icon,"朋友圈"),
-                new GridModel(R.drawable.share_layout_qq_icon,"QQ"),
-                new GridModel(R.drawable.share_layout_wx_icon,"微信"),
-                new GridModel(R.drawable.share_layout_friends_icon,"朋友圈")
+                new GridModel(R.drawable.share_layout_qq_icon, "QQ"),
+                new GridModel(R.drawable.share_layout_wx_icon, "微信"),
+                new GridModel(R.drawable.share_layout_friends_icon, "朋友圈"),
+                new GridModel(R.drawable.share_layout_qq_icon, "QQ"),
+                new GridModel(R.drawable.share_layout_wx_icon, "微信"),
+                new GridModel(R.drawable.share_layout_friends_icon, "朋友圈")
         };
     }
-    private String[] getSimpleStringData(int size){
+
+    private String[] getSimpleStringData(int size) {
         String[] array = new String[size];
-        for (int i= 0;i< size ;i++){
+        for (int i = 0; i < size; i++) {
             array[i] = "SIMPLE-DATA" + i;
         }
         return array;
     }
+
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -156,14 +161,33 @@ public class BottomDialogActivity extends AppCompatActivity implements View.OnCl
                 bottomSheetDlg.show();
                 break;
             case R.id.button4:
-                 startActivity(new Intent(this,BottomSheetActivity.class));
+                startActivity(new Intent(this, BottomSheetActivity.class));
+                break;
+            case R.id.button5:
+                List<PopBean> pops = new ArrayList<>();
+                for (int i = 0; i < 5; i++) {
+                    PopBean pop = new PopBean("item" + i, 0);
+                    pops.add(pop);
+                }
+                ListPopWindow popWindow = new ListPopWindow(this, this, this, v, pops, "cancel", "title");
+                popWindow.showAtLocation(v, Gravity.CENTER | Gravity.BOTTOM, 0, 0);
                 break;
         }
     }
 
     @Override
     public void onItemClick(MasterDialog dialog, View view, int position) {
-        Toast.makeText(this,"Item click:" + position,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Item click:" + position, Toast.LENGTH_SHORT).show();
         dialog.dismiss();
+    }
+
+    @Override
+    public void onBottomClick() {
+        Toast.makeText(BottomDialogActivity.this, "bottom click", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onPopItemClick(View view, int position) {
+        Toast.makeText(BottomDialogActivity.this, "pos:" + position, Toast.LENGTH_SHORT).show();
     }
 }
